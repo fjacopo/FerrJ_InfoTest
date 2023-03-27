66bf27f273795560464564cf770d14c49fd313e8
ferrari.19318 <ferrari.19318@LABI1-6PC08>
2023-03-27 08:39:45 +0200

    pt2

---
+++ FerrJ_InfoTest/FerrJ_Candidato.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FerrJ_InfoTest
+{
+    public abstract class FerrJ_Candidato
+    {
+        private int FerrJ_Matricola;
+        private string FerrJ_Nome;
+
+
+        public int Matricola
+        {
+            get { return FerrJ_Matricola; }
+            set { FerrJ_Matricola = value; }
+        }
+
+        public string Nome
+        {
+            get { return FerrJ_Nome; }
+            set { FerrJ_Nome = value; }
+        }
+        public FerrJ_Candidato()
+        {
+            FerrJ_Matricola = 0;
+            FerrJ_Nome = null;
+        }
+        public FerrJ_Candidato(int n)
+        {
+            FerrJ_Matricola = n;
+            FerrJ_Nome = null;
+        }
+        public FerrJ_Candidato(string s)
+        {
+            FerrJ_Matricola = 0;
+            FerrJ_Nome = s;
+        }
+        public FerrJ_Candidato(int n, string s)
+        {
+            FerrJ_Matricola = n;
+            FerrJ_Nome = s;
+        }
+
+
+        public abstract bool isIdoneo();
+
+        public abstract int punteggio();
+        
+    }
+}
+
+

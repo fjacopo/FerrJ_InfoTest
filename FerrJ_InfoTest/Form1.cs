0ed7ae369820dc99c5f044d057d7c2c656d90d10
ferrari.19318 <ferrari.19318@LABI1-6PC08>
2023-03-27 10:58:09 +0200

    pt6

--- FerrJ_InfoTest/Form1.cs
+++ FerrJ_InfoTest/Form1.cs
@@ -15,8 +15,64 @@ namespace FerrJ_InfoTest
         public Form1()
         {
             InitializeComponent();
-            List<FerrJ_Candidato> Candidati = new List<FerrJ_Candidato>();
         }
-        
+        List<FerrJ_Candidato> Candidati = new List<FerrJ_Candidato>();
+        public void Aggiungi(FerrJ_Candidato a)
+        {
+            Candidati.Add(a);
+        }
+        public string[] Visualizza(List<FerrJ_Candidato> c)
+        {
+            string[] s = new string[999];
+            int count = 0;
+
+            foreach (FerrJ_Candidato a in Candidati)
+            {
+                s[count] = a.ToString();
+                count++;
+            }
+            return s;
+        }
+        public void Modifica(string nome, int numero)
+        {
+            for (int i = 0; i < Candidati.Count; i++)
+            {
+                if (Candidati[i].Matricola == numero)
+                {
+                    Candidati[i].Nome = nome;
+                }
+            }
+        }
+        public void Elimina(int numero)
+        {
+            for (int i = 0; i < Candidati.Count; i++)
+            {
+                if (Candidati[i].Matricola == numero)
+                {
+                    Candidati[i] = null;
+                }
+            }
+        }
+        public string[] VisualizzaIdonei()
+        {
+            string[] s = new string[999];
+            int n = 0;
+            foreach (FerrJ_Candidato d in Candidati)
+            {
+                bool b = d.isIdoneo();
+                if (b == true)
+                {
+                    s[n] = d.ToString();
+                    n++;
+                }
+            }
+            return s;
+        }
+        public void Ordinamento()
+        {
+            //non ho avuto tempo :(
+        }
+
+
     }
 }
